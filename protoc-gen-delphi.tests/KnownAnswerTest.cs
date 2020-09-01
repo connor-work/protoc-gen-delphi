70abbe925c86f6610b3db2d41bb4d676013fc4ff
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-01 09:43:13 +0200

    Create stub for protoc-gen-delphi and known-answer test with empty schema as initial test data

---
+++ protoc-gen-delphi.tests/KnownAnswerTest.cs
@@ -0,0 +1,65 @@
+using Google.Protobuf;
+using Google.Protobuf.Compiler;
+using Google.Protobuf.Reflection;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Tests
+{
+    /// <summary>
+    /// Tests <see cref="ProtocGenDelphi"/> with known inputs and outputs.
+    /// </summary>
+    public class KnownAnswerTest
+    {
+        /// <summary>
+        /// Formatter settings for encoding protobuf messages as JSON for test data. Can be used when creating new test vectors.
+        /// </summary>
+        public static readonly JsonFormatter.Settings protobufJsonFormatSettings = JsonFormatter.Settings.Default.WithFormatDefaultValues(false).WithFormatEnumsAsIntegers(false);
+
+        /// <summary>
+        /// Parser settings for decoding protobuf messages from JSON for test data
+        /// </summary>
+        public static readonly JsonParser.Settings protobufJsonParseSettings = JsonParser.Settings.Default.WithIgnoreUnknownFields(false);
+
+        /// <summary>
+        /// File name extension (without leading dot) for JSON-encoded protobuf messages in test data
+        /// </summary>
+        public static readonly string protobufJsonFileExtension = "pb.json";
+
+        /// <summary>
+        /// File name extension (without leading dot) for JSON-encoded <see cref="CodeGeneratorRequest">s in test data
+        /// </summary>
+        public static readonly string requestFileExtension = "request." + protobufJsonFileExtension;
+
+        /// <summary>
+        /// File name extension (without leading dot) for JSON-encoded <see cref="CodeGeneratorResponse">s in test data
+        /// </summary>
+        public static readonly string responseFileExtension = "response." + protobufJsonFileExtension;
+
+        /// <summary>
+        /// <see cref="ProtocGenDelphi"/> handles a request with the expected response.
+        /// </summary>
+        /// <param name="request">Incoming request from <c>protoc</c></param>
+        /// <param name="expectedResponse">Expected response</param>
+        [Theory]
+        [MemberData(nameof(KnownResponses))]
+        public void ProducesExpectedResponse(CodeGeneratorRequest request, CodeGeneratorResponse expectedResponse) => Assert.Equal(expectedResponse, new ProtocGenDelphi().HandleRequest(request));
+
+        public static IEnumerable<object[]> KnownResponses()
+        {
+            JsonParser jsonParser = new JsonParser(protobufJsonParseSettings);
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            // Resources contain pairs of JSON-encoded request and expected response messages
+            foreach (string requestFileName in assembly.GetManifestResourceNames().Where(name => name.EndsWith(requestFileExtension)))
+            {
+                string responseFileName = requestFileName.Substring(0, requestFileName.Length - requestFileExtension.Length) + responseFileExtension;
+                using StreamReader responseReader = new StreamReader(assembly.GetManifestResourceStream(responseFileName) ?? throw new FileNotFoundException(responseFileName));
+                using StreamReader requestReader = new StreamReader(assembly.GetManifestResourceStream(requestFileName));
+                yield return new object[] { jsonParser.Parse<CodeGeneratorRequest>(requestReader), jsonParser.Parse<CodeGeneratorResponse>(responseReader) };
+            }
+        }
+    }
+}
---
+++ protoc-gen-delphi/ProtocGenDelphi.cs
@@ -0,0 +1,45 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.Compiler;
+using System;
+using System.IO;
+
+namespace Work.Connor.Protobuf.Delphi.ProtocGenDelphi
+{
+    /// <summary>
+    /// Plug-in for the protobuf compiler <c>protoc</c> that generates Delphi unit source code files for protobuf schema definitions.
+    /// </summary>
+    public class ProtocGenDelphi
+    {
+        /// <summary>
+        /// File name extension (without leading dot) for protobuf schema definitions
+        /// </summary>
+        public static readonly string protoFileExtension = "proto";
+
+        /// <summary>
+        /// File name extension (without leading dot) for generated Delphi unit source files
+        /// </summary>
+        public static readonly string unitSourceFileExtension = "pas";
+
+        static void Main(string[] args)
+        {
+            // protoc communicates with the plug-in through stdin and stdout
+            using Stream input = Console.OpenStandardInput();
+            using Stream output = Console.OpenStandardOutput();
+            CodeGeneratorRequest request = CodeGeneratorRequest.Parser.ParseFrom(input);
+            ProtocGenDelphi plugIn = new ProtocGenDelphi();
+            CodeGeneratorResponse response = plugIn.HandleRequest(request);
+            response.WriteTo(output);
+        }
+
+        /// <summary>
+        /// Handles a request from code generation that <c>protoc</c> passed to the plug-in,
+        /// by generating a response message.
+        /// </summary>
+        /// <param name="request">The request from <c>protoc</c></param>
+        /// <returns>The response to <c>protoc</c></returns>
+        public CodeGeneratorResponse HandleRequest(CodeGeneratorRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
