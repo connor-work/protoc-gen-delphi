57be665336772cae5524757e05066c83602e7842
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-02 18:49:01 +0200

    Add test for protobuf message type compilation

---
+++ protoc-gen-delphi.tests/known-answer-test-vectors/uMessages.pas
@@ -0,0 +1,18 @@
+unit uMessages;
+
+interface
+
+uses
+  Com.GitHub.Pikaju.Protobuf.Delphi.uMessage;
+
+type
+  TMessageX = class(TProtobufMessage)
+  end;
+
+type
+  TMessageY = class(TProtobufMessage)
+  end;
+
+implementation
+
+end.
