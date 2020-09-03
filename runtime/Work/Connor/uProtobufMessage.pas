459f3a7e628775a7197dc0f48372f42e76b0dd31
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-03 19:39:37 +0200

    Add stub runtime for testing and as reference

---
+++ runtime/Work/Connor/uProtobufMessage.pas
@@ -0,0 +1,102 @@
+unit Work.Connor.Protobuf.Delphi.StubRuntime.uProtobufMessage;
+
+{$IFDEF FPC}
+  {$MODE DELPHI}
+{$ENDIF}
+
+interface
+
+uses
+  SysUtils;
+
+type
+  /// <summary>
+  /// Common ancestor of all generated classes that represent protobuf message types.
+  /// </summary>
+  /// <remarks>
+  /// Can be used directly to handle message of unknown type.
+  /// The message instance carries transitive ownership of embedded objects in protobuf field values,
+  /// and is responsible for their deallocation.
+  /// </remarks>
+  TProtobufMessage = class
+
+  public
+    /// <summary>
+    /// Constructs an empty message with all protobuf fields absent (i.e., set to their default values).
+    /// </summary>
+    /// <remarks>
+    /// Protobuf's interpretation of the absence of a field may be counterintuitive for Delphi developers.
+    /// For a detailed explanation, see https://developers.google.com/protocol-buffers/docs/proto3#default.
+    /// </remarks>
+    constructor Create; virtual;
+
+    /// <summary>
+    /// Destroys the message and all objects and resources held by it (e.g., protobuf field values).
+    /// </summary>
+    /// <remarks>
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    destructor Destroy; override;
+
+    /// <summary>
+    /// Renders all protobuf fields absent by setting them to their default values.
+    /// </summary>
+    /// <remarks>
+    /// The resulting instance state is equivalent to a newly constructed empty message.
+    /// For more details, see the documentation of <c>Create</c>.
+    /// This procedure may cause the destruction of transitively owned objects.
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    procedure Clear; virtual;
+
+    /// <summary>
+    /// Encodes the message using the protobuf binary wire format and writes it to a stream.
+    /// </summary>
+    /// <param name="aDest">The stream that the encoded message is written to</param>
+    procedure Encode(aDest: TStream);
+
+    /// <summary>
+    /// Fills the message's protobuf fields by decoding the message using the protobuf binary wire format from data that is read from a stream.
+    /// </summary>
+    /// <param name="aSource">The stream that the data is read from</param>
+    /// <remarks>
+    /// Protobuf fields that are not present in the read data are rendered absent by setting them to their default values.
+    /// This may cause the destruction of transitively owned objects (this is also the case when a present fields overwrites a previous value).
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    procedure Decode(aSource: TStream);
+  end;
+
+implementation
+
+procedure NotImplementedInStub;
+begin
+  raise Exception.Create('This functionality is not implemented by the stub runtime');
+end;
+
+constructor TProtobufMessage.Create;
+begin
+  NotImplementedInStub;
+end;
+
+destructor TProtobufMessage.Destroy;
+begin
+  NotImplementedInStub;
+end;
+
+procedure TProtobufMessage.Clear;
+begin
+  NotImplementedInStub;
+end;
+
+procedure TProtobufMessage.Encode(aDest: TStream);
+begin
+  NotImplementedInStub;
+end;
+
+procedure TProtobufMessage.Decode(aSource: TStream);
+begin
+  NotImplementedInStub;
+end;
+
+end.
