d3024ee46c741944291b0d5774983820649e4a5b
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-04 17:31:26 +0200

    Add message class skeleton procedure inheritance and ClearOwnFields to tests

--- protoc-gen-delphi.tests/known-answer-test-vectors/messages.output_folder/uMessages.pas
+++ protoc-gen-delphi.tests/known-answer-test-vectors/messages.output_folder/uMessages.pas
@@ -16,6 +16,8 @@ type
   public procedure Encode(aDest: TStream); override;
 
   public procedure Decode(aSource: TStream); override;
+
+  private procedure ClearOwnFields;
   end;
 
 type
@@ -29,47 +31,71 @@ type
   public procedure Encode(aDest: TStream); override;
 
   public procedure Decode(aSource: TStream); override;
+
+  private procedure ClearOwnFields;
   end;
 
 implementation
 
 constructor TMessageX.Create;
 begin
+  inherited;
+  ClearOwnFields;
 end;
 
 destructor TMessageX.Destroy;
 begin
+  inherited;
 end;
 
 procedure TMessageX.Clear;
 begin
+  inherited;
+  ClearOwnFields;
 end;
 
 procedure TMessageX.Encode(aDest: TStream);
 begin
+  inherited;
 end;
 
 procedure TMessageX.Decode(aSource: TStream);
+begin
+  inherited;
+end;
+
+procedure TMessageX.ClearOwnFields;
 begin
 end;
 
 constructor TMessageY.Create;
 begin
+  inherited;
+  ClearOwnFields;
 end;
 
 destructor TMessageY.Destroy;
 begin
+  inherited;
 end;
 
 procedure TMessageY.Clear;
 begin
+  inherited;
+  ClearOwnFields;
 end;
 
 procedure TMessageY.Encode(aDest: TStream);
 begin
+  inherited;
 end;
 
 procedure TMessageY.Decode(aSource: TStream);
+begin
+  inherited;
+end;
+
+procedure TMessageY.ClearOwnFields;
 begin
 end;
 
