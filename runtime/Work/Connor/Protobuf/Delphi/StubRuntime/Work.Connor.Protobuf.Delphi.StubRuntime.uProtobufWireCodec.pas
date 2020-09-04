083037148aae04bce95f3c909dfcd8b1ff2ad480
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-04 17:29:24 +0200

    Start definition of field encoding interface in stub runtime

---
+++ runtime/Work/Connor/Protobuf/Delphi/StubRuntime/Work.Connor.Protobuf.Delphi.StubRuntime.uProtobufWireCodec.pas
@@ -0,0 +1,15 @@
+unit Work.Connor.Protobuf.Delphi.StubRuntime.uProtobufRepeatedField;
+
+{$IFDEF FPC}
+  {$MODE DELPHI}
+{$ENDIF}
+
+interface
+
+type
+  TProtobufRepeatedField<T> = class
+  end;
+
+implementation
+
+end.
