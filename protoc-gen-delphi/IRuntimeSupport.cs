16fd99f84499a2317e2ece906b2116ec48ad0228
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-08 14:53:49 +0200

    QV5-34 Missing change from last commit

--- protoc-gen-delphi/IRuntimeSupport.cs
+++ protoc-gen-delphi/IRuntimeSupport.cs
@@ -99,6 +99,7 @@ namespace Work.Connor.Protobuf.Delphi.ProtocGenDelphi
                 {
                     FieldDescriptorProto.Types.Type.String => "uProtobufString",
                     FieldDescriptorProto.Types.Type.Uint32 => "uProtobufUint32",
+                    FieldDescriptorProto.Types.Type.Enum => "uProtobufEnum",
                     _ => throw new NotImplementedException()
                 });
             }
