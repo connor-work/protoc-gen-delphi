9a95db7093c1507450b5f24a44dc43d31dbf0632
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-10 14:51:23 +0200

    QV5-34 Correct test vector namespace_from_path

--- protoc-gen-delphi.tests/test-vectors/known-protoc-outputs/namespace_from_path.protoc-output/SpaceX/SpaceY/SpaceX.SpaceY.uInputFileNamespaceFromPath.pas
+++ protoc-gen-delphi.tests/test-vectors/known-protoc-outputs/namespace_from_path.protoc-output/SpaceX/SpaceY/SpaceX.SpaceY.uInputFileNamespaceFromPath.pas
@@ -1,7 +1,7 @@
 /// <remarks>
 /// This unit corresponds to the protobuf schema definition (.proto file) <c>space_x/space_y/input_file_namespace_from_path.proto</c>.
 /// </remarks>
-unit SpaceX.SpaceX.uInputFileNamespaceFromPath;
+unit SpaceX.SpaceY.uInputFileNamespaceFromPath;
 
 interface
 
