89a34e7df5e0135d9a92f100b126d7b6b4bc6398
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-11 13:42:23 +0200

    rebase test

---
+++ protoc-gen-delphi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Work.Connor.Protobuf.Delphi.ProtocGenDelphi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
