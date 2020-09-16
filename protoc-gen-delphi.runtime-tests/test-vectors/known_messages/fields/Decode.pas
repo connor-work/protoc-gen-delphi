44a3199a8886ffa56787f22da098a2b3166aa142
Julien Scholz <julien.scholz@sotax.com>
2020-09-16 16:57:50 +0200

    QV5-124 Add fields runtime test vectors

---
+++ protoc-gen-delphi.runtime-tests/test-vectors/known_messages/fields/Decode.pas
@@ -0,0 +1,24 @@
+program Decode;
+
+{$IFDEF FPC}
+  {$MODE DELPHI}
+{$ENDIF}
+
+uses
+  Com.GitHub.Pikaju.Protobuf.Delphi.Test.uProtobufTestUtility,
+  uFields;
+
+var
+  lStdIn: THandleStream;
+  lMessage: TMessageX;
+begin
+  lStdIn := THandleStream.Create(GetStdHandle(STD_INPUT_HANDLE));
+
+  lMessage := TMessageX.Create;
+  lMessage.Decode(lStdIn);
+  AssertTrue(lMessage.FieldX = 133742069, 'Int32 is parsed correctly.');
+  AssertTrue(lMessage.FieldY = 'スゲーデン so uberuhaputo', 'String is parsed correctly.');
+  lMessage.Free;
+
+  lStdIn.Free;
+end.
