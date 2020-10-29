012bb381784ae1c3891d2997be0d0e4ee8cb59e7
Connor Röhricht (connor.work) <connor@connor.work>
2020-10-29 22:36:04 +0100

    #45 Added basic stubs for protobuf message type support

---
+++ runtime/Work/Connor/Protobuf/Delphi/ProtocGenDelphi/Runtime/Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.uProtobufMessage.pas
@@ -0,0 +1,58 @@
+/// Copyright 2020 Connor Roehricht (connor.work)
+/// Copyright 2020 Sotax AG
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+/// 
+///     http://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+/// <summary>
+/// Runtime support for protobuf message types.
+/// </summary>
+/// <remarks>
+/// This unit provides access to all runtime classes and interfaces that implement protobuf message types.
+/// Client code may need to reference it in order to operate on generic protobuf messages.
+/// </remarks>
+unit Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.uProtobufMessage;
+
+interface
+
+uses
+  // To expose IProtobufMessage
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.uIProtobufMessage,
+  // RUNTIME-IMPL: Replace reference
+  // To expose TProtobufMessage
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.StubRuntime.uProtobufMessage;
+
+type
+  /// <summary>
+  /// Common interface of all generated classes that represent protobuf message types.
+  /// </summary>
+  /// <remarks>
+  /// Can be used directly to handle messages of unknown type.
+  /// The message instance carries transitive ownership of embedded objects in protobuf field values,
+  /// and is responsible for their deallocation.
+  /// </remarks>
+  IProtobufMessage = Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.uIProtobufMessage.IProtobufMessage;
+
+  // RUNTIME-IMPL: Replace type
+  /// <summary>
+  /// Common ancestor of all generated classes that represent protobuf message types.
+  /// </summary>
+  /// <remarks>
+  /// Can be used directly to handle messages of unknown type.
+  /// The message instance carries transitive ownership of embedded objects in protobuf field values,
+  /// and is responsible for their deallocation.
+  /// </remarks>
+  TProtobufMessage = Work.Connor.Protobuf.Delphi.ProtocGenDelphi.StubRuntime.uProtobufMessage.TProtobufMessage;
+
+implementation
+
+end.
---
+++ runtime/Work/Connor/Protobuf/Delphi/ProtocGenDelphi/StubRuntime/Work.Connor.Protobuf.Delphi.ProtocGenDelphi.StubRuntime.uProtobufMessage.pas
@@ -0,0 +1,166 @@
+/// Copyright 2020 Connor Roehricht (connor.work)
+/// Copyright 2020 Sotax AG
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+/// 
+///     http://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+/// <summary>
+/// Runtime library support code for protobuf message types.
+/// </summary>
+/// <remarks>
+/// This unit defines the common ancestor of all generated classes representing protobuf message types, <see cref="TProtobufMessage"/>.
+/// Client code should reference it indirectly through <see cref="N:Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.uProtobufMessage"/>.
+/// </remarks>
+unit Work.Connor.Protobuf.Delphi.ProtocGenDelphi.StubRuntime.uProtobufMessage;
+
+interface
+
+uses
+  // To implement IProtobufMessage
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.uIProtobufMessage,
+  // To implement IProtobufMessageInternal
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uIProtobufMessageInternal,
+  // TStream for encoding and decoding of messages
+  System.Classes,
+  // Stub runtime helper code
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.StubRuntime.uStubRuntime;
+
+type
+  /// <summary>
+  /// Common ancestor of all generated classes that represent protobuf message types.
+  /// </summary>
+  /// <remarks>
+  /// Can be used directly to handle messages of unknown type.
+  /// The message instance carries transitive ownership of embedded objects in protobuf field values,
+  /// and is responsible for their deallocation.
+  /// </remarks>
+  TProtobufMessage = class(TInterfacedPersistent, IProtobufMessage, IProtobufMessageInternal)
+  public
+    /// <summary>
+    /// Constructs an empty message with all protobuf fields absent, meaning that they are set to their default values.
+    /// </summary>
+    /// <remarks>
+    /// Protobuf's interpretation of the absence of a field may be counterintuitive for Delphi developers.
+    /// For a detailed explanation, see https://developers.google.com/protocol-buffers/docs/proto3#default.
+    /// </remarks>
+    constructor Create; virtual;
+
+    /// <summary>
+    /// Destroys the message and all objects and resources held by it, including the protobuf field values.
+    /// </summary>
+    /// <remarks>
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    destructor Destroy; override;
+
+    // IProtobufMessage implementation
+
+    /// <summary>
+    /// Renders all protobuf fields absent by setting them to their default values.
+    /// </summary>
+    /// <remarks>
+    /// The resulting instance state is equivalent to a newly constructed empty message.
+    /// For more details, see the documentation of <see cref="Create"/>.
+    /// This procedure may cause the destruction of transitively owned objects.
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    procedure Clear; virtual;
+
+    /// <summary>
+    /// Encodes the message using the protobuf binary wire format and writes it to a stream.
+    /// </summary>
+    /// <param name="aDest">The stream that the encoded message is written to</param>
+    /// <remarks>
+    /// Since the protobuf binary wire format does not include length information for top-level messages,
+    /// the recipient may not be able to detect the end of the message when reading it from a stream.
+    /// If this is required, use <see cref="EncodeDelimited"/> instead.
+    /// </remarks>
+    procedure Encode(aDest: TStream);
+
+    /// <summary>
+    /// Encodes the message using the protobuf binary wire format and writes it to a stream, prefixed with length information.
+    /// </summary>
+    /// <param name="aDest">The stream that the encoded message is written to</param>
+    /// <remarks>
+    /// Unlike <see cref="Encode"/>, this method enables the recipient to detect the end of the message by decoding it using
+    /// <see cref="DecodeDelimited"/>.
+    /// </remarks>
+    procedure EncodeDelimited(aDest: TStream);
+
+    /// <summary>
+    /// Fills the message's protobuf fields by decoding the message using the protobuf binary wire format from data that is read from a stream.
+    /// Data is read until <see cref="TStream.Read"/> returns 0.
+    /// </summary>
+    /// <param name="aSource">The stream that the data is read from</param>
+    /// <remarks>
+    /// Protobuf fields that are not present in the read data are rendered absent by setting them to their default values.
+    /// This may cause the destruction of transitively owned objects (this is also the case when a present fields overwrites a previous value).
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// This method should not be used on streams where the actual size of their contents may not be known yet (this might result in data loss).
+    /// If this is required, use <see cref="DecodeDelimited"/> instead.
+    /// </remarks>
+    procedure Decode(aSource: TStream);
+
+    /// <summary>
+    /// Fills the message's protobuf fields by decoding the message using the protobuf binary wire format from data that is read from a stream.
+    /// The data must be prefixed with message length information, as implemented by <see cref="EncodeDelimited"/>.
+    /// </summary>
+    /// <param name="aSource">The stream that the data is read from</param>
+    /// <remarks>
+    /// See remarks on <see cref="Decode">.
+    /// </remarks>
+    procedure DecodeDelimited(aSource: TStream);
+
+    // IProtobufMessageInternal implementation
+
+    // TODO
+  end;
+
+implementation
+
+constructor TProtobufMessage.Create;
+begin
+  NotImplementedInStub;
+end;
+
+destructor TProtobufMessage.Destroy;
+begin
+  NotImplementedInStub;
+end;
+
+procedure TProtobufMessage.Clear;
+begin
+  NotImplementedInStub;
+end;
+
+procedure TProtobufMessage.Encode(aDest: TStream);
+begin
+  NotImplementedInStub;
+end;
+
+
+procedure TProtobufMessage.EncodeDelimited(aDest: TStream);
+begin
+  NotImplementedInStub;
+end;
+
+procedure TProtobufMessage.Decode(aSource: TStream);
+begin
+  NotImplementedInStub;
+end;
+
+procedure TProtobufMessage.DecodeDelimited(aSource: TStream);
+begin
+  NotImplementedInStub;
+end;
+
+end.
