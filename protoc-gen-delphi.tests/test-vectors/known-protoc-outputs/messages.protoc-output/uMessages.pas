f15e217417b6b60b79c7e646b894898bb4754733
Connor Röhricht (connor.work) <connor@connor.work>
2020-11-01 22:24:15 +0100

    #45 Added some known-answer test outputs to RAD studio test project and adapted them to match new API and code style

--- protoc-gen-delphi.tests/test-vectors/known-protoc-outputs/messages.protoc-output/uMessages.pas
+++ protoc-gen-delphi.tests/test-vectors/known-protoc-outputs/messages.protoc-output/uMessages.pas
@@ -18,56 +18,74 @@ type
   /// This class corresponds to the protobuf message type <c>MessageX</c>.
   /// </remarks>
   TMessageX = class(TProtobufMessage)
-  /// <summary>
-  /// Creates an empty <see cref="TMessageX"/> that can be used as a protobuf message.
-  /// Initially, all protobuf fields are absent, meaning that they are set to their default values.
-  /// </summary>
-  /// <remarks>
-  /// Protobuf's interpretation of the absence of a field may be counterintuitive for Delphi developers.
-  /// For a detailed explanation, see https://developers.google.com/protocol-buffers/docs/proto3#default.
-  /// </remarks>
-  public constructor Create; override;
+    /// <summary>
+    /// Creates an empty <see cref="TMessageX"/> that can be used as a protobuf message.
+    /// Initially, all protobuf fields are absent, meaning that they are set to their default values.
+    /// </summary>
+    /// <remarks>
+    /// Protobuf's interpretation of the absence of a field may be counterintuitive for Delphi developers.
+    /// For a detailed explanation, see https://developers.google.com/protocol-buffers/docs/proto3#default.
+    /// </remarks>
+    public constructor Create; override;
 
-  /// <summary>
-  /// Destroys the instances and all objects and resources held by it, including the protobuf field values.
-  /// </summary>
-  /// <remarks>
-  /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
-  /// </remarks>
-  public destructor Destroy; override;
+    /// <summary>
+    /// Destroys the instances and all objects and resources held by it, including the protobuf field values.
+    /// </summary>
+    /// <remarks>
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    public destructor Destroy; override;
 
-  /// <summary>
-  /// Renders all protobuf fields absent by setting them to their default values.
-  /// </summary>
-  /// <remarks>
-  /// The resulting instance state is equivalent to a newly constructed <see cref="TMessageX"/>.
-  /// For more details, see the documentation of <see cref="Create"/>.
-  /// This procedure may cause the destruction of transitively owned objects.
-  /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
-  /// </remarks>
-  public procedure Clear; override;
+    /// <summary>
+    /// Renders all protobuf fields absent by setting them to their default values.
+    /// </summary>
+    /// <remarks>
+    /// The resulting instance state is equivalent to a newly constructed <see cref="TMessageX"/>.
+    /// For more details, see the documentation of <see cref="Create"/>.
+    /// This procedure may cause the destruction of transitively owned objects.
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    public procedure Clear; override;
 
-  /// <summary>
-  /// Encodes the message using the protobuf binary wire format and writes it to a stream.
-  /// </summary>
-  /// <param name="aDest">The stream that the encoded message is written to</param>
-  public procedure Encode(aDest: TStream); override;
+    /// <summary>
+    /// Encodes the message using the protobuf binary wire format and writes it to a stream.
+    /// </summary>
+    /// <param name="aDest">The stream that the encoded message is written to</param>
+    public procedure Encode(aDest: TStream); override;
 
-  /// <summary>
-  /// Fills the message's protobuf fields by decoding the message using the protobuf binary wire format from data that is read from a stream.
-  /// </summary>
-  /// <param name="aSource">The stream that the data is read from</param>
-  /// <remarks>
-  /// Protobuf fields that are not present in the read data are rendered absent by setting them to their default values.
-  /// This may cause the destruction of transitively owned objects (this is also the case when a present fields overwrites a previous value)
-  /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
-  /// </remarks>
-  public procedure Decode(aSource: TStream); override;
+    /// <summary>
+    /// Fills the message's protobuf fields by decoding the message using the protobuf binary wire format from data that is read from a stream.
+    /// </summary>
+    /// <param name="aSource">The stream that the data is read from</param>
+    /// <remarks>
+    /// Protobuf fields that are not present in the read data are rendered absent by setting them to their default values.
+    /// This may cause the destruction of transitively owned objects (this is also the case when a present fields overwrites a previous value)
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    public procedure Decode(aSource: TStream); override;
 
-  /// <summary>
-  /// Renders those protobuf fields absent that belong to <see cref="TMessageX"/> (i.e., are not managed by an ancestor class), by setting them to their default values.
-  /// </summary>
-  private procedure ClearOwnFields;
+    /// <summary>
+    /// Copies the protobuf data from another object to this one.
+    /// </summary>
+    /// <param name="aSource">Object to copy from</param>
+    /// <remarks>
+    /// The other object must be a protobuf message of the same type.
+    /// This performs a deep copy; hence, no ownership is shared.
+    /// This procedure may cause the destruction of transitively owned objects in this message instance.
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    public procedure Assign(aSource: TPersistent); override;
+
+    /// <summary>
+    /// Renders those protobuf fields absent that belong to <see cref="TMessageX"/> (i.e., are not managed by an ancestor class), by setting them to their default values.
+    /// </summary>
+    private procedure ClearOwnFields;
+
+    /// <summary>
+    /// Copies those protobuf fields that belong to <see cref="TMessageX"/> (i.e., are not managed by an ancestor class), during a call to <see cref="TInterfacedPersistent.Assign"/>.
+    /// </summary>
+    /// <param name="aSource">Source message to copy from</param>
+    private procedure AssignOwnFields(aSource: TMessageX);
   end;
 
 type
@@ -75,56 +93,74 @@ type
   /// This class corresponds to the protobuf message type <c>MessageY</c>.
   /// </remarks>
   TMessageY = class(TProtobufMessage)
-  /// <summary>
-  /// Creates an empty <see cref="TMessageY"/> that can be used as a protobuf message.
-  /// Initially, all protobuf fields are absent, meaning that they are set to their default values.
-  /// </summary>
-  /// <remarks>
-  /// Protobuf's interpretation of the absence of a field may be counterintuitive for Delphi developers.
-  /// For a detailed explanation, see https://developers.google.com/protocol-buffers/docs/proto3#default.
-  /// </remarks>
-  public constructor Create; override;
+    /// <summary>
+    /// Creates an empty <see cref="TMessageY"/> that can be used as a protobuf message.
+    /// Initially, all protobuf fields are absent, meaning that they are set to their default values.
+    /// </summary>
+    /// <remarks>
+    /// Protobuf's interpretation of the absence of a field may be counterintuitive for Delphi developers.
+    /// For a detailed explanation, see https://developers.google.com/protocol-buffers/docs/proto3#default.
+    /// </remarks>
+    public constructor Create; override;
 
-  /// <summary>
-  /// Destroys the instances and all objects and resources held by it, including the protobuf field values.
-  /// </summary>
-  /// <remarks>
-  /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
-  /// </remarks>
-  public destructor Destroy; override;
+    /// <summary>
+    /// Destroys the instances and all objects and resources held by it, including the protobuf field values.
+    /// </summary>
+    /// <remarks>
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    public destructor Destroy; override;
 
-  /// <summary>
-  /// Renders all protobuf fields absent by setting them to their default values.
-  /// </summary>
-  /// <remarks>
-  /// The resulting instance state is equivalent to a newly constructed <see cref="TMessageY"/>.
-  /// For more details, see the documentation of <see cref="Create"/>.
-  /// This procedure may cause the destruction of transitively owned objects.
-  /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
-  /// </remarks>
-  public procedure Clear; override;
+    /// <summary>
+    /// Renders all protobuf fields absent by setting them to their default values.
+    /// </summary>
+    /// <remarks>
+    /// The resulting instance state is equivalent to a newly constructed <see cref="TMessageY"/>.
+    /// For more details, see the documentation of <see cref="Create"/>.
+    /// This procedure may cause the destruction of transitively owned objects.
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    public procedure Clear; override;
 
-  /// <summary>
-  /// Encodes the message using the protobuf binary wire format and writes it to a stream.
-  /// </summary>
-  /// <param name="aDest">The stream that the encoded message is written to</param>
-  public procedure Encode(aDest: TStream); override;
+    /// <summary>
+    /// Encodes the message using the protobuf binary wire format and writes it to a stream.
+    /// </summary>
+    /// <param name="aDest">The stream that the encoded message is written to</param>
+    public procedure Encode(aDest: TStream); override;
 
-  /// <summary>
-  /// Fills the message's protobuf fields by decoding the message using the protobuf binary wire format from data that is read from a stream.
-  /// </summary>
-  /// <param name="aSource">The stream that the data is read from</param>
-  /// <remarks>
-  /// Protobuf fields that are not present in the read data are rendered absent by setting them to their default values.
-  /// This may cause the destruction of transitively owned objects (this is also the case when a present fields overwrites a previous value)
-  /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
-  /// </remarks>
-  public procedure Decode(aSource: TStream); override;
+    /// <summary>
+    /// Fills the message's protobuf fields by decoding the message using the protobuf binary wire format from data that is read from a stream.
+    /// </summary>
+    /// <param name="aSource">The stream that the data is read from</param>
+    /// <remarks>
+    /// Protobuf fields that are not present in the read data are rendered absent by setting them to their default values.
+    /// This may cause the destruction of transitively owned objects (this is also the case when a present fields overwrites a previous value)
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    public procedure Decode(aSource: TStream); override;
 
-  /// <summary>
-  /// Renders those protobuf fields absent that belong to <see cref="TMessageY"/> (i.e., are not managed by an ancestor class), by setting them to their default values.
-  /// </summary>
-  private procedure ClearOwnFields;
+    /// <summary>
+    /// Copies the protobuf data from another object to this one.
+    /// </summary>
+    /// <param name="aSource">Object to copy from</param>
+    /// <remarks>
+    /// The other object must be a protobuf message of the same type.
+    /// This performs a deep copy; hence, no ownership is shared.
+    /// This procedure may cause the destruction of transitively owned objects in this message instance.
+    /// Developers must ensure that no shared ownership of current field values or further nested embedded objects is held.
+    /// </remarks>
+    public procedure Assign(aSource: TPersistent); override;
+
+    /// <summary>
+    /// Renders those protobuf fields absent that belong to <see cref="TMessageY"/> (i.e., are not managed by an ancestor class), by setting them to their default values.
+    /// </summary>
+    private procedure ClearOwnFields;
+
+    /// <summary>
+    /// Copies those protobuf fields that belong to <see cref="TMessageY"/> (i.e., are not managed by an ancestor class), during a call to <see cref="TInterfacedPersistent.Assign"/>.
+    /// </summary>
+    /// <param name="aSource">Source message to copy from</param>
+    private procedure AssignOwnFields(aSource: TMessageY);
   end;
 
 implementation
@@ -156,7 +192,20 @@ begin
   inherited;
 end;
 
+procedure TMessageX.Assign(aSource: TPersistent);
+var
+  lSource: TMessageX;
+begin
+  lSource := aSource as TMessageX;
+  inherited Assign(lSource);
+  AssignOwnFields(lSource);
+end;
+
 procedure TMessageX.ClearOwnFields;
+begin
+end;
+
+procedure TMessageX.AssignOwnFields(aSource: TMessageX);
 begin
 end;
 
@@ -187,7 +236,20 @@ begin
   inherited;
 end;
 
+procedure TMessageY.Assign(aSource: TPersistent);
+var
+  lSource: TMessageY;
+begin
+  lSource := aSource as TMessageY;
+  inherited Assign(lSource);
+  AssignOwnFields(lSource);
+end;
+
 procedure TMessageY.ClearOwnFields;
+begin
+end;
+
+procedure TMessageY.AssignOwnFields(aSource: TMessageY);
 begin
 end;
 
