7012face6dab472d66ed3e37f5f1058a0d40cfc1
Connor Erdmann (connor.work) <connor@connor.work>
2025-11-25 21:27:14 +0100

    Updated the copyright information of some recently changed scripts

--- script/github-actions-deploy.ps1
+++ script/github-actions-deploy.ps1
@@ -1,4 +1,4 @@
-# Copyright 2020 Connor Roehricht (connor.work)
+# Copyright 2025 Connor Roehricht (connor.work)
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
