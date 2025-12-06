3eb9c487ae581e3db25a5cb863de196276e2ed90
Connor Erdmann (connor.work) <connor@connor.work>
2025-12-06 10:44:39 +0100

    #85 Transferred runtime code for supporting sint64
    Got permission from Julien Scholz to re-license runtime code from 2020 under Apache License 2.0 (was MIT).

--- runtime/Work/Connor/Protobuf/Delphi/ProtocGenDelphi/Runtime/Internal/Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufSint64.pas
+++ runtime/Work/Connor/Protobuf/Delphi/ProtocGenDelphi/Runtime/Internal/Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufSint64.pas
@@ -1,4 +1,5 @@
-/// Copyright 2020 Connor Roehricht (connor.work)
+/// Copyright 2025 Connor Erdmann (connor.work)
+/// Copyright 2020 Julien Scholz
 /// Copyright 2020 Sotax AG
 /// 
 /// Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,9 +31,8 @@ interface
 uses
   // Runtime-internal support for the protobuf binary wire format
   Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uIProtobufWireCodec,
-  // RUNTIME-IMPL: Replace reference
-  // To provide the wire codec instance
-  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.StubRuntime.uProtobufWireCodec;
+  // To implement TProtobufVarintWireCodec<Int64>
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufVarintWireCodec;
 
 var
   /// <summary>
@@ -41,12 +41,59 @@ var
   /// </summary>
   gProtobufWireCodecSint64: IProtobufWireCodec<Int64>;
 
+type
+  /// <summary>
+  /// Runtime library implementation of <see cref="T:IProtobufWireCodec"/> for the protobuf type <c>sint64</c>.
+  /// </summary>
+  TProtobufSint64WireCodec = class(TProtobufVarintWireCodec<Int64>)
+    // TProtobufVarintWireCodec<Int64> implementation
+
+    public
+      function FromUInt64(aValue: UInt64): Int64; override;
+      function ToUInt64(aValue: Int64): UInt64; override;
+
+    // TProtobufWireCodec<Int64> implementation
+    
+    public
+      function GetDefault: Int64; override;
+  end;
+
 implementation
+
+uses
+  // For protobuf default values
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.uProtobuf;
+
+// TProtobufVarintWireCodec<Int64> implementation
+
+function TProtobufSint64WireCodec.FromUInt64(aValue: UInt64): Int64;
+var
+  lZigZagValue: UInt64;
+begin
+  ValidateBounds(aValue, 64, False);
+  lZigZagValue := UInt64(aValue);
+  result := (lZigZagValue shr 1) xor -(lZigZagValue and 1);
+end;
+
+function TProtobufSint64WireCodec.ToUInt64(aValue: Int64): UInt64;
+var
+  lZigZagValue: UInt64;
+begin
+  lZigZagValue := aValue shl 1;
+  if (aValue < 0) then lZigZagValue := lZigZagValue xor -1;
+  result := UInt64(lZigZagValue);
+end;
+
+// TProtobufWireCodec<Int64> implementation
+
+function TProtobufSint64WireCodec.GetDefault: Int64;
+begin
+  result := PROTOBUF_DEFAULT_VALUE_SINT64;
+end;
 
 initialization
 begin
-  // RUNTIME-IMPL: Replace constructed class
-  gProtobufWireCodecSint64 := TProtobufWireCodec<Int64>.Create;
+  gProtobufWireCodecSint64 := TProtobufSint64WireCodec.Create;
 end;
 
 end.
