bcbece22addad940906df93c08e6f3a0f1552e2f
Connor Erdmann (connor.work) <connor@connor.work>
2025-12-06 13:05:10 +0100

    #85 Transferred runtime code for supporting double
    Got permission from Julien Scholz to re-license runtime code from 2020 under Apache License 2.0 (was MIT).

--- runtime/Work/Connor/Protobuf/Delphi/ProtocGenDelphi/Runtime/Internal/Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufDouble.pas
+++ runtime/Work/Connor/Protobuf/Delphi/ProtocGenDelphi/Runtime/Internal/Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufDouble.pas
@@ -1,4 +1,5 @@
-/// Copyright 2020 Connor Roehricht (connor.work)
+/// Copyright 2025 Connor Erdmann (connor.work)
+/// Copyright 2020 Julien Scholz
 /// Copyright 2020 Sotax AG
 /// 
 /// Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,9 +31,16 @@ interface
 uses
   // Runtime-internal support for the protobuf binary wire format
   Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uIProtobufWireCodec,
-  // RUNTIME-IMPL: Replace reference
-  // To provide the wire codec instance
-  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.StubRuntime.uProtobufWireCodec;
+  // To implement TProtobufFixedWidthWireCodec<Single>
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufFixedWidthWireCodec,
+  // For wire type
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufTag,
+  // TStream for encoding of messages
+{$IFDEF WORK_CONNOR_DELPHI_COMPILER_UNIT_SCOPE_NAMES}
+  System.Classes;
+{$ELSE}
+  Classes;
+{$ENDIF}
 
 var
   /// <summary>
@@ -41,12 +49,63 @@ var
   /// </summary>
   gProtobufWireCodecDouble: IProtobufWireCodec<Double>;
 
+type
+  /// <summary>
+  /// Runtime library implementation of <see cref="T:IProtobufWireCodec"/> for the protobuf type <c>double</c>.
+  /// </summary>
+  TProtobufDoubleWireCodec = class(TProtobufFixedWidthWireCodec<Double>)
+    // TProtobufFixedWidthWireCodec<Double> implementation
+
+    public
+      function GetWireType: TProtobufWireType; override;
+      function DecodeValue(aSource: TStream): Double; override;
+      procedure EncodeValue(aValue: Double; aDest: TStream); override;
+
+    // TProtobufWireCodec<Double> implementation
+    
+    public
+      function GetDefault: Double; override;
+      function IsDefault(aValue: Double): Boolean; override;
+  end;
+
 implementation
+
+uses
+  // For protobuf default values
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.uProtobuf;
+
+// TProtobufFixedWidthWireCodec<Double> implementation
+
+function TProtobufDoubleWireCodec.GetWireType: TProtobufWireType;
+begin
+  result := wt64Bit;
+end;
+
+function TProtobufDoubleWireCodec.DecodeValue(aSource: TStream): Double;
+begin
+  aSource.Read(result, SizeOf(result));
+end;
+
+procedure TProtobufDoubleWireCodec.EncodeValue(aValue: Double; aDest: TStream);
+begin
+  aDest.Write(aValue, SizeOf(aValue));
+end;
+
+// TProtobufWireCodec<Double> implementation
+
+function TProtobufDoubleWireCodec.GetDefault: Double;
+begin
+  result := PROTOBUF_DEFAULT_VALUE_DOUBLE;
+end;
+
+function TProtobufDoubleWireCodec.IsDefault(aValue: Double): Boolean;
+begin
+  result := aValue = GetDefault;
+end;
 
 initialization
 begin
-  // RUNTIME-IMPL: Replace constructed class
-  gProtobufWireCodecDouble := TProtobufWireCodec<Double>.Create;
+  gProtobufWireCodecDouble := TProtobufDoubleWireCodec.Create;
 end;
 
 end.
