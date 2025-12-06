4b2f72b8407a138d4c193733ecf286b328d1a02f
Connor Erdmann (connor.work) <connor@connor.work>
2025-12-06 11:05:19 +0100

    #85 Transferred runtime code for supporting sfixed64
    Got permission from Julien Scholz to re-license runtime code from 2020 under Apache License 2.0 (was MIT).

--- runtime/Work/Connor/Protobuf/Delphi/ProtocGenDelphi/Runtime/Internal/Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufSfixed64.pas
+++ runtime/Work/Connor/Protobuf/Delphi/ProtocGenDelphi/Runtime/Internal/Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufSfixed64.pas
@@ -1,4 +1,5 @@
-/// Copyright 2020 Connor Roehricht (connor.work)
+/// Copyright 2025 Connor Erdmann (connor.work)
+/// Copyright 2020 Julien Scholz
 /// Copyright 2020 Sotax AG
 /// 
 /// Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,9 +31,16 @@ interface
 uses
   // Runtime-internal support for the protobuf binary wire format
   Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uIProtobufWireCodec,
-  // RUNTIME-IMPL: Replace reference
-  // To provide the wire codec instance
-  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.StubRuntime.uProtobufWireCodec;
+  // To implement TProtobufFixedWidthWireCodec<Int64>
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufFixedWidthWireCodec,
+  // For wire type
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.Runtime.Internal.uProtobufTag,
+  // TStream for encoding of messages
+{$IFDEF WORK_CONNOR_DELPHI_COMPILER_UNIT_SCOPE_NAMES}
+  System.Classes;
+{$ELSE}
+  Classes;
+{$ENDIF}
 
 var
   /// <summary>
@@ -41,12 +49,63 @@ var
   /// </summary>
   gProtobufWireCodecSfixed64: IProtobufWireCodec<Int64>;
 
+type
+  /// <summary>
+  /// Runtime library implementation of <see cref="T:IProtobufWireCodec"/> for the protobuf type <c>sfixed64</c>.
+  /// </summary>
+  TProtobufSfixed64WireCodec = class(TProtobufFixedWidthWireCodec<Int64>)
+    // TProtobufFixedWidthWireCodec<Int64> implementation
+
+    public
+      function GetWireType: TProtobufWireType; override;
+      function DecodeValue(aSource: TStream): Int64; override;
+      procedure EncodeValue(aValue: Int64; aDest: TStream); override;
+
+    // TProtobufWireCodec<Int64> implementation
+    
+    public
+      function GetDefault: Int64; override;
+      function IsDefault(aValue: Int64): Boolean; override;
+  end;
+
 implementation
+
+uses
+  // For protobuf default values
+  Work.Connor.Protobuf.Delphi.ProtocGenDelphi.uProtobuf;
+
+// TProtobufFixedWidthWireCodec<Int64> implementation
+
+function TProtobufSfixed64WireCodec.GetWireType: TProtobufWireType;
+begin
+  result := wt64Bit;
+end;
+
+function TProtobufSfixed64WireCodec.DecodeValue(aSource: TStream): Int64;
+begin
+  aSource.Read(result, SizeOf(result));
+end;
+
+procedure TProtobufSfixed64WireCodec.EncodeValue(aValue: Int64; aDest: TStream);
+begin
+  aDest.Write(aValue, SizeOf(aValue));
+end;
+
+// TProtobufWireCodec<Int64> implementation
+
+function TProtobufSfixed64WireCodec.GetDefault: Int64;
+begin
+  result := PROTOBUF_DEFAULT_VALUE_SFIXED64;
+end;
+
+function TProtobufSfixed64WireCodec.IsDefault(aValue: Int64): Boolean;
+begin
+  result := aValue = GetDefault;
+end;
 
 initialization
 begin
-  // RUNTIME-IMPL: Replace constructed class
-  gProtobufWireCodecSfixed64 := TProtobufWireCodec<Int64>.Create;
+  gProtobufWireCodecSfixed64 := TProtobufSfixed64WireCodec.Create;
 end;
 
 end.
